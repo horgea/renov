614fb8a0d1593306a368c3ffc80e25496cff9fdb
Calin Horgea <calin.horgea@gmail.com>
2020-08-08 18:11:55 +0200

    update script configuration

--- cypress/integration/Complicated test scenarios.js
+++ cypress/integration/Complicated test scenarios.js
@@ -1,85 +1,82 @@
-import {range} from "../core/utils";
+import { range } from "../core/utils";
 
 describe('Test some complicated scenarios', () => {
-    before(()=> {
+    beforeEach(() => {
         cy.viewport('macbook-13')
         cy.setCookie('eu_cookie_consent', '%7B%22a%22%3A%7B%22estat%22%3A%5B%22customSavedPresentation%22%2C%22userSelection-list%22%2C%22navtree-user-settings%22%5D%2C%22europa%22%3A%5B%22all_documented%22%5D%7D%2C%22r%22%3A%7B%7D%7D')
-    })
-
-    beforeEach(() => {
         cy.server()
         cy.route('**/GEO/getCodeListJson/**').as('myTable') //last request before page is loaded
-    
+
         //Go to required dataset view
-         cy.visit('/eurostat/databrowser/view/T2020_10/default/table?lang=en')
-    
-         //wait for required Table to load on page
-         cy.wait('@myTable', {timeout: 15000})
-         cy.get('#loading-bar-spinner', {timeout: 10000})
+        cy.visit('/eurostat/databrowser/view/T2020_10/default/table?lang=en')
+
+        //wait for required Table to load on page
+        cy.wait('@myTable', { timeout: 15000 })
+        cy.get('#loading-bar-spinner', { timeout: 10000 })
             .should('not.exist', 'Wait for page to load')
     })
 
     it('1. Check Custom Format - Static Positions - From-to', () => {
-     
-     cy.contains('Create custom dataset')
-        .should('be.visible')
-        .click()
 
-     cy.contains('Define your custom dataset')
-        .should('be.visible')
+        cy.contains('Create custom dataset')
+            .should('be.visible')
+            .click()
 
-     cy.contains('Static positions')
-        .should('be.visible')
-        .click()
+        cy.contains('Define your custom dataset')
+            .should('be.visible')
 
-     cy.contains('From-to')  //select Filter
-        .should('be.visible')
-        .click()
+        cy.contains('Static positions')
+            .should('be.visible')
+            .click()
 
-     cy.get('#rangeFromId')
-        .children()
-        .first()
-        .then(fromItem => {
-            const fromValue = parseInt(fromItem.text())
-            
-            cy.get('#rangeToId')
-                .children()
-                .first()
-                .then(toItem =>{
-                const toValue = parseInt(toItem.text())
-                
-                cy.get('#dimension-4')
+        cy.contains('From-to')  //select Filter
+            .should('be.visible')
+            .click()
+
+        cy.get('#rangeFromId')
+            .children()
+            .first()
+            .then(fromItem => {
+                const fromValue = parseInt(fromItem.text())
+
+                cy.get('#rangeToId')
                     .children()
                     .first()
-                    .should('contain', 'Time')
-                    .then(item => {
-                    const requiredItems = parseInt(item.text().split('/')[1].split(')')[0])
-                    
-                    //scroll and save the found values to a List
-                    let mySet = new Set();   
-                    
-                    const myRange = range(1, requiredItems, 16); //how many times I need to scroll
-                    cy.wrap(myRange)
-                        .each(() => {
-                            cy.get('.md-virtual-repeat-offsetter')
-                                .find('.label')
-                                .each(item => {
-                                    let myYear = parseInt(item.text())
-                                    expect(myYear).to.be.within(fromValue, toValue)
-                                    mySet.add(myYear)
-                                })
-                                cy.get('.md-virtual-repeat-offsetter')
-                                    .find('.label')
-                                    .last()
-                                    .scrollIntoView()
-                                    .wait(500)
-                            })
+                    .then(toItem => {
+                        const toValue = parseInt(toItem.text())
 
-                            cy.wrap(mySet)
-                                .should('have.length', requiredItems, 'Total rows in table')
-                })
+                        cy.get('#dimension-4')
+                            .children()
+                            .first()
+                            .should('contain', 'Time')
+                            .then(item => {
+                                const requiredItems = parseInt(item.text().split('/')[1].split(')')[0])
+
+                                //scroll and save the found values to a List
+                                let mySet = new Set();
+
+                                const myRange = range(1, requiredItems, 16); //how many times I need to scroll
+                                cy.wrap(myRange)
+                                    .each(() => {
+                                        cy.get('.md-virtual-repeat-offsetter')
+                                            .find('.label')
+                                            .each(item => {
+                                                let myYear = parseInt(item.text())
+                                                expect(myYear).to.be.within(fromValue, toValue)
+                                                mySet.add(myYear)
+                                            })
+                                        cy.get('.md-virtual-repeat-offsetter')
+                                            .find('.label')
+                                            .last()
+                                            .scrollIntoView()
+                                            .wait(500)
+                                    })
+
+                                cy.wrap(mySet)
+                                    .should('have.length', requiredItems, 'Total rows in table')
+                            })
+                    })
             })
-        })
     })
 
     it('2. Check Change Format of Decimal Symbol to Comma', () => {
@@ -90,21 +87,21 @@ describe('Test some complicated scenarios', () => {
         // get all available cells and count the Dots
         cy.get('.cell-value')
             .then(allCells => {
-            const currentDots = allCells.text().match(/[.]/g).length
-        
-            cy.contains('comma')
-                .click()
+                const currentDots = allCells.text().match(/[.]/g).length
 
-            cy.get('#loading-bar-spinner')
-                .should('not.exist')
+                cy.contains('comma')
+                    .click()
 
-            //get all available cells and count the Commas 
-            cy.get('.cell-value')
-            .then(allCells => {
-            const currentCommas = allCells.text().match(/[,]/g).length
+                cy.get('#loading-bar-spinner')
+                    .should('not.exist')
 
-            expect(currentDots).to.eql(currentCommas, 'All Dots have changed to Commas')
+                //get all available cells and count the Commas 
+                cy.get('.cell-value')
+                    .then(allCells => {
+                        const currentCommas = allCells.text().match(/[,]/g).length
+
+                        expect(currentDots).to.eql(currentCommas, 'All Dots have changed to Commas')
+                    })
             })
-        })
     })
 });
